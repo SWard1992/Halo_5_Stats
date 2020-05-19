b6ba4b90fcd95b792671e1c8f1fdf4cce7cd2f24
VN040397 <sean_ward@hmin.honda.com>
2020-05-19 09:51:15 -0400

    added pulling some playlist data

--- main.py
+++ main.py
@@ -31,6 +31,38 @@ profileDict = response.json()
 testUser = profileData(user,profileDict.get('ServiceTag'),profileDict.get('Company').get('Name'),profileDict.get('Company').get('Id'))
 print(testUser.gamerTag, testUser.serviceTag,testUser.companyId, testUser.companyName)
 
+class playlistData:
+    def __init__(self, name, description, isRanked, imageUrl, gameMode, isActive, id, contentId):
+        self.name = name
+        self.description = description
+        self.isRanked = isRanked
+        self.imageUrl = imageUrl
+        self.gameMode = gameMode
+        self.isActive = isActive
+        self.id = id
+        self.contentId = contentId
+
+playlistURL = '/metadata/h5/metadata/playlists'
+response = requests.get(connectionUrl+playlistURL, headers=headers)
+playlists = response.json() 
+playlistList = []
+count = 0
+for x in playlists:
+    match = playlistData(playlists[count].get('name'),playlists[count].get('description'),playlists[count].get('isRanked'),playlists[count].get('imageUrl'),playlists[count].get('gameMode'),playlists[count].get('isActive'),playlists[count].get('id'),playlists[count].get('contentId'))
+    playlistList.append(match)
+    count = count + 1
+print(len(playlistList))
+for gameType in playlistList:
+    print(gameType.name,gameType.description)
+
+"""mStatsUrl = '/stats/h5/servicerecords/arena?players=' + testUser.gamerTag
+response = requests.get(connectionUrl+mStatsUrl, headers=headers)
+matchStatsDict = response.json()
+print(matchStatsDict.keys())
+print(matchStatsDict.get('Results'))
+"""
+
+
 
 
 

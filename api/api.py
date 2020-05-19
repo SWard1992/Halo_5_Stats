8ac196a565fc63224a90b9dacc4fb1081dc4fb93
Alex Johnson <alex.johnson24@outlook.com>
2020-05-19 18:13:54 -0400

    correct typo

--- api/api.py
+++ api/api.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def demo():
-    return "<h2>Use URL structure 'http://127.0.0.1:5000/profiles/<user> to lookup user data</h2>"
+    return "<h2>Use URL structure 'http://127.0.0.1:5000/profiles/user' to lookup user data</h2>"
 
 @app.route('/profiles/<user>', methods=['GET'])
 def getUserProfile(user=None):
